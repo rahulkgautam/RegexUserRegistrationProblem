09bde7eb7a5c1fb4cb4ff2c06316873db73a3a98
rahulkgautam <rahulkgautam125@gmail.com>
2023-08-01 22:04:28 +0530

    User First alphabet capital and min 3 character

---
+++ UserRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexRegistration
+{
+    class UserRegistration
+    {
+        public static void UserFirstName()
+        {
+            string firstName = "Rahul";
+
+            string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+            bool isValid = Regex.IsMatch(firstName, pattern);
+
+            Console.WriteLine($"First Name is {firstName},Valid {isValid}");
+        }
+    }
+}
