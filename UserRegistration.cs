7ddea1e47670bd438660a29adbd890590cb6cf77
rahulkgautam <rahulkgautam125@gmail.com>
2023-08-01 22:57:41 +0530

    User Password in 1 capital letter For all Rule Pass

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -11,7 +11,7 @@ namespace RegexRegistration
         {
             string password = "Password@123"; 
 
-            string pattern = @"^.{8,}$";
+            string pattern = @"^(?=.*[A-Z]).{8,}";
             bool isValid = Regex.IsMatch(password, pattern);
 
             Console.WriteLine($"Password is {password},Valid {isValid}");
